2673b42244a430c6051b4bde097252c758f35347
Kaz Sasa <kazssym@vx68k.org>
2012-06-27 22:14:18 +0900

    Refreshed the project.

--- src/mp2enc/About.h
+++ src/mp2enc/About.h
@@ -2,16 +2,16 @@
 #ifndef AboutH
 #define AboutH
 //----------------------------------------------------------------------------
-#include <vcl\System.hpp>
-#include <vcl\Windows.hpp>
-#include <vcl\SysUtils.hpp>
-#include <vcl\Classes.hpp>
-#include <vcl\Graphics.hpp>
-#include <vcl\Forms.hpp>
-#include <vcl\Controls.hpp>
-#include <vcl\StdCtrls.hpp>
-#include <vcl\Buttons.hpp>
-#include <vcl\ExtCtrls.hpp>
+#include <System.hpp>
+#include <Windows.hpp>
+#include <SysUtils.hpp>
+#include <Classes.hpp>
+#include <Graphics.hpp>
+#include <Forms.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Buttons.hpp>
+#include <ExtCtrls.hpp>
 //----------------------------------------------------------------------------
 class TAboutBox : public TForm
 {
