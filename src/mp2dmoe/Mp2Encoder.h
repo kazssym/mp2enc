d379159241e5561a1cee4d5b664d2c0fa9907a75
Kaz Sasa <kazssym@vx68k.org>
2013-03-19 23:05:29 +0900

    Created a new header file Mp2Encoder.h.

---
+++ src/mp2dmoe/Mp2Encoder.h
@@ -0,0 +1,28 @@
+/*
+ * CLSID definition for Mp2Encoder.
+ * Copyright (C) 2010-2013  Kaz Sasa
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef Mp2EncoderH
+#define Mp2EncoderH 1
+
+#include <guiddef.h>
+
+/* {3FCD30F6-88FE-4A44-A518-1DBB0937DAE4} */
+DEFINE_GUID(CLSID_Mp2Encoder, 0x3FCD30F6, 0x88FE, 0x4A44, 0xA5, 0x18,
+        0x1D, 0xBB, 0x09, 0x37, 0xDA, 0xE4);
+
+#endif
