a4198084410c3d15bf72187f881cf5d81a69cf24
Kaz Sasa <kazssym@vx68k.org>
2013-03-20 17:58:44 +0900

    Added skeleton implementation for TMp2EncoderImpl.

--- src/mp2dmoe/Mp2EncoderImpl.cpp
+++ src/mp2dmoe/Mp2EncoderImpl.cpp
@@ -1,52 +1,150 @@
-// ---------------------------------------------------------------------------
-// MP2ENCODERIMPL : Implementation of TMp2EncoderImpl (CoClass: Mp2Encoder, Interface: IMp2Encoder)
-// ---------------------------------------------------------------------------
+/*
+ * Mp2EncoderImpl - C++ class for Mp2Encoder (implementation)
+ * Copyright (C) 2010-2013  Kaz Sasa
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <vcl.h>
 #pragma hdrstop
 
+#include <initguid.h>
 #include "Mp2EncoderImpl.h"
 
+#include "Mp2Encoder.h"
+#include <System.Win.ComServ.hpp>
 
-// ---------------------------------------------------------------------------
-// TMp2EncoderImpl
-// ---------------------------------------------------------------------------
-__fastcall TMp2EncoderImpl::TMp2EncoderImpl()
-{
+static void CreateFactory() {
+    new TComObjectFactory(GetComServer(), __classid(TMp2EncoderImpl),
+            CLSID_Mp2Encoder, L"TMp2EncoderImpl", L"MP2 Encoder DMO",
+            ciInternal, Comobj::tmBoth);
+}
+#pragma startup CreateFactory 64
+
+// TMp2EncoderImpl implementation
+
+__fastcall TMp2EncoderImpl::TMp2EncoderImpl() {
 }
 
-
-// ---------------------------------------------------------------------------
-// TMp2EncoderImpl
-// ---------------------------------------------------------------------------
-__fastcall TMp2EncoderImpl::TMp2EncoderImpl(const System::_di_IInterface Controller)
-                              : inherited(Controller)
-{
+__fastcall TMp2EncoderImpl::TMp2EncoderImpl
+        (const _di_IInterface Controller)
+        : inherited(Controller) {
 }
 
-
-// ---------------------------------------------------------------------------
-// TMp2EncoderImpl
-// ---------------------------------------------------------------------------
-__fastcall TMp2EncoderImpl::TMp2EncoderImpl(Comobj::TComObjectFactory* Factory,
-                                      const System::_di_IInterface Controller)
-                              : inherited(Factory, Controller)
-{
+__fastcall TMp2EncoderImpl::TMp2EncoderImpl(TComObjectFactory *Factory,
+        const _di_IInterface Controller)
+        : inherited(Factory, Controller) {
 }
 
-// ---------------------------------------------------------------------------
-// TMp2EncoderImpl - Class Factory
-// ---------------------------------------------------------------------------
-static void createFactory()
-{
-  new TCppComObjectFactory<TMp2EncoderImpl>(Comserv::GetComServer(),
-                           __classid(TMp2EncoderImpl),
-                           CLSID_Mp2Encoder,
-                           "TMp2EncoderImpl",
-                           "",
-                           Comobj::ciMultiInstance,
-                           Comobj::tmBoth);
+STDMETHODIMP TMp2EncoderImpl::GetStreamCount(DWORD *pcInputStreams,
+        DWORD *pcOutputStreams) {
+    return E_NOTIMPL;
 }
-#pragma startup createFactory 32
 
+STDMETHODIMP TMp2EncoderImpl::GetInputStreamInfo(DWORD dwInputStreamIndex,
+        DWORD *pdwFlags) {
+    return E_NOTIMPL;
+}
 
+STDMETHODIMP TMp2EncoderImpl::GetOutputStreamInfo
+        (DWORD dwOutputStreamIndex, DWORD *pdwFlags) {
+    return E_NOTIMPL;
+}
 
+STDMETHODIMP TMp2EncoderImpl::GetInputType(DWORD dwInputStreamIndex,
+        DWORD dwTypeIndex, DMO_MEDIA_TYPE *pmt) {
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP TMp2EncoderImpl::GetOutputType(DWORD dwOutputStreamIndex,
+        DWORD dwTypeIndex, DMO_MEDIA_TYPE *pmt){
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP TMp2EncoderImpl::SetInputType(DWORD dwInputStreamIndex,
+        const DMO_MEDIA_TYPE *pmt, DWORD dwFlags){
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP TMp2EncoderImpl::SetOutputType(DWORD dwOutputStreamIndex,
+        const DMO_MEDIA_TYPE *pmt, DWORD dwFlags){
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP TMp2EncoderImpl::GetInputCurrentType(DWORD dwInputStreamIndex,
+        DMO_MEDIA_TYPE *pmt){
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP TMp2EncoderImpl::GetOutputCurrentType(DWORD dwOutputStreamIndex,
+        DMO_MEDIA_TYPE *pmt){
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP TMp2EncoderImpl::GetInputSizeInfo(DWORD dwInputStreamIndex,
+        DWORD *pcbSize, DWORD *pcbMaxLookahead, DWORD *pcbAlignment) {
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP TMp2EncoderImpl::GetOutputSizeInfo(DWORD dwOutputStreamIndex,
+        DWORD *pcbSize, DWORD *pcbAlignment) {
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP TMp2EncoderImpl::GetInputMaxLatency(DWORD dwInputStreamIndex,
+        REFERENCE_TIME *prtMaxLatency){
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP TMp2EncoderImpl::SetInputMaxLatency(DWORD dwInputStreamIndex,
+        REFERENCE_TIME rtMaxLatency){
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP TMp2EncoderImpl::Flush(){
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP TMp2EncoderImpl::Discontinuity(DWORD dwInputStreamIndex) {
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP TMp2EncoderImpl::AllocateStreamingResources(){
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP TMp2EncoderImpl::FreeStreamingResources(){
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP TMp2EncoderImpl::GetInputStatus(DWORD dwInputStreamIndex,
+        DWORD *dwFlags){
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP TMp2EncoderImpl::ProcessInput(DWORD dwInputStreamIndex,
+        IMediaBuffer *pBuffer, DWORD dwFlags, REFERENCE_TIME rtTimestamp,
+        REFERENCE_TIME rtTimelength){
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP TMp2EncoderImpl::ProcessOutput(DWORD dwFlags,
+        DWORD cOutputBufferCount, DMO_OUTPUT_DATA_BUFFER *pOutputBuffers,
+        DWORD *pdwStatus){
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP TMp2EncoderImpl::Lock(LONG bLock){
+    return E_NOTIMPL;
+}
--- src/mp2dmoe/Mp2EncoderImpl.h
+++ src/mp2dmoe/Mp2EncoderImpl.h
@@ -1,36 +1,98 @@
-// ---------------------------------------------------------------------------
-// Mp2EncoderImpl.h : Declaration of the TMp2EncoderImpl
-// ---------------------------------------------------------------------------
+/*
+ * Mp2EncoderImpl - C++ class for Mp2Encoder (interface)
+ * Copyright (C) 2010-2013  Kaz Sasa
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef Mp2EncoderImplH
 #define Mp2EncoderImplH
 
-#include <ComServ.hpp>
+#include <dmo.h>
 #include <axbase.h>
-#include "mp2dmoe_TLB.h"
 
-// ---------------------------------------------------------------------------
-// TMp2EncoderImpl     Implements IMp2Encoder, default interface of Mp2Encoder
-// ThreadingModel : tmBoth
-// Dual Interface : FALSE
-// Event Support  : FALSE
-// Description    : 
-// ---------------------------------------------------------------------------
-class DAX_COM_CLASS TMp2EncoderImpl : public TCppComObject<IMp2Encoder>
-{
-  typedef _COM_CLASS inherited;
+#if __cplusplus < 201103L
+#define override
+#endif
 
-public:
-  __fastcall TMp2EncoderImpl();
-  __fastcall TMp2EncoderImpl(const System::_di_IInterface Controller);
-  __fastcall TMp2EncoderImpl(Comobj::TComObjectFactory* Factory, const System::_di_IInterface Controller);
-  
+namespace Mp2encoderimpl {
+    /*
+     * Mp2Encoder implementation.
+     */
+    class DAX_COM_CLASS TMp2EncoderImpl : public TComObject, public IInterface,
+            public IMediaObject {
+        typedef TComObject inherited;
 
-  // IMp2Encoder
-protected:
-};
+    public:
+        __fastcall TMp2EncoderImpl();
+        __fastcall TMp2EncoderImpl(const System::_di_IInterface Controller);
+        __fastcall TMp2EncoderImpl(System::Win::Comobj::TComObjectFactory *Factory,
+                const System::_di_IInterface Controller);
 
+        // IMediaObject methods
+        virtual HRESULT STDMETHODCALLTYPE GetStreamCount(DWORD *pcInputStreams,
+                DWORD *pcOutputStreams) override;
+        virtual HRESULT STDMETHODCALLTYPE GetInputStreamInfo
+                (DWORD dwInputStreamIndex, DWORD *pdwFlags) override;
+        virtual HRESULT STDMETHODCALLTYPE GetOutputStreamInfo
+                (DWORD dwOutputStreamIndex, DWORD *pdwFlags) override;
+        virtual HRESULT STDMETHODCALLTYPE GetInputType(DWORD dwInputStreamIndex,
+                DWORD dwTypeIndex, DMO_MEDIA_TYPE *pmt) override;
+        virtual HRESULT STDMETHODCALLTYPE GetOutputType
+                (DWORD dwOutputStreamIndex, DWORD dwTypeIndex,
+                DMO_MEDIA_TYPE *pmt) override;
+        virtual HRESULT STDMETHODCALLTYPE SetInputType(DWORD dwInputStreamIndex,
+                const DMO_MEDIA_TYPE *pmt, DWORD dwFlags) override;
+        virtual HRESULT STDMETHODCALLTYPE SetOutputType
+                (DWORD dwOutputStreamIndex, const DMO_MEDIA_TYPE *pmt,
+                DWORD dwFlags) override;
+        virtual HRESULT STDMETHODCALLTYPE GetInputCurrentType
+                (DWORD dwInputStreamIndex, DMO_MEDIA_TYPE *pmt) override;
+        virtual HRESULT STDMETHODCALLTYPE GetOutputCurrentType
+                (DWORD dwOutputStreamIndex, DMO_MEDIA_TYPE *pmt) override;
+        virtual HRESULT STDMETHODCALLTYPE GetInputSizeInfo
+                (DWORD dwInputStreamIndex, DWORD *pcbSize,
+                DWORD *pcbMaxLookahead, DWORD *pcbAlignment) override;
+        virtual HRESULT STDMETHODCALLTYPE GetOutputSizeInfo
+                (DWORD dwOutputStreamIndex, DWORD *pcbSize, DWORD *pcbAlignment)
+                override;
+        virtual HRESULT STDMETHODCALLTYPE GetInputMaxLatency
+                (DWORD dwInputStreamIndex, REFERENCE_TIME *prtMaxLatency)
+                override;
+        virtual HRESULT STDMETHODCALLTYPE SetInputMaxLatency
+                (DWORD dwInputStreamIndex, REFERENCE_TIME rtMaxLatency)
+                override;
+        virtual HRESULT STDMETHODCALLTYPE Flush() override;
+        virtual HRESULT STDMETHODCALLTYPE Discontinuity
+                (DWORD dwInputStreamIndex) override;
+        virtual HRESULT STDMETHODCALLTYPE AllocateStreamingResources() override;
+        virtual HRESULT STDMETHODCALLTYPE FreeStreamingResources() override;
+        virtual HRESULT STDMETHODCALLTYPE GetInputStatus
+                (DWORD dwInputStreamIndex, DWORD *dwFlags) override;
+        virtual HRESULT STDMETHODCALLTYPE ProcessInput(DWORD dwInputStreamIndex,
+                IMediaBuffer *pBuffer, DWORD dwFlags,
+                REFERENCE_TIME rtTimestamp, REFERENCE_TIME rtTimelength)
+                override;
+        virtual HRESULT STDMETHODCALLTYPE ProcessOutput(DWORD dwFlags,
+                DWORD cOutputBufferCount,
+                DMO_OUTPUT_DATA_BUFFER *pOutputBuffers, DWORD *pdwStatus)
+                override;
+        virtual HRESULT STDMETHODCALLTYPE Lock(LONG bLock) override;
 
+        DAX_IMPL_IUNKNOWN(inherited);
+    };
+}
+using namespace Mp2encoderimpl;
 
-
-#endif //Mp2EncoderImplH
-
+#endif
